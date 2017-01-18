af3fe57906f87ff32cee6f0d9c4e261267a16b77
Jens Giessmann <jg@handcode.de>
2017-01-18 23:51:31 +0100

    foramtted graph, add comp_ratio graph

--- templates/check_mk-zvolsize.php
+++ templates/check_mk-zvolsize.php
@@ -8,11 +8,6 @@
 
 # Define some colors ..
 #
-$_WARNRULE = '#FFFF00';
-$_CRITRULE = '#FF0000';
-$_AREA     = '#256aef';
-$_LINE     = '#000000';
-#
 # Initial Logic ...
 #
 
@@ -28,107 +23,60 @@ foreach ($NAME as $i => $n) {
     $ACT[$n]  = $ACT[$i];
 }
 
+$zvol_name = explode(' ', $this->MACRO['DISP_SERVICEDESC'])[0];
+
+
+# get logenst name for indenting
+$name_lenght = 0;
+foreach ($this->DS as $KEY=>$VAL) {
+    if ($name_lenght < strlen($VAL['NAME'])) {
+        $name_lenght = strlen($VAL['NAME']);
+    }
+}
 #
 # graph with all data
 #
 $ds_name[$i] = "vol-sizes";
 $def[$i]  = "";
-$opt[$i]  = " --vertical-label 'Vol-Sizes in MB' --title '" . $this->MACRO['DISP_HOSTNAME'] . " / " . $this->MACRO['DISP_SERVICEDESC'] . "' -l 0";
+#$opt[$i]  = " --vertical-label 'Vol-Sizes in MB' --title '" . $this->MACRO['DISP_HOSTNAME'] . " / " . $this->MACRO['DISP_SERVICEDESC'] . "' -l 0";
+$opt[$i]  = " --vertical-label 'ZVol-Sizes in MB' --title '" . $this->MACRO['DISP_HOSTNAME'] . " / " . $zvol_name . "' -l 0";
 
 # possible vars are:
 # size|used|free|used_data|used_snap|comp_ratio
 
 foreach ($this->DS as $KEY=>$VAL) {
-    if (preg_match('/^(size|used|used_data|used_snap|comp_ratio)$/', $VAL['NAME'])) {
+    if (preg_match('/^(size|used|used_data|used_snap)$/', $VAL['NAME'])) {
+        $name = str_pad($VAL['NAME'], $name_lenght);
         $def[$i] .= "DEF:var${KEY}=${VAL['RRDFILE']}:${DS[$VAL['DS']]}:AVERAGE ";
-        #$def[$i] .= "AREA:var${KEY}".rrd::color($KEY).":\"".$VAL['NAME']."\":STACK ";
-        $def[$i] .= "LINE:var${KEY}".rrd::color($KEY).":\"".$VAL['NAME']."\" ";
-        $def[$i] .= "GPRINT:var${KEY}:LAST:\"Last %5.0lf%S\" ";
-        $def[$i] .= "GPRINT:var${KEY}:MAX:\"Max %5.0lf%S\" ";
-        $def[$i] .= "GPRINT:var${KEY}:AVERAGE:\"Average %5.1lf%S\" ";
+        #$def[$i] .= "AREA:var${KEY}".rrd::color($KEY).":\"". $name ."\":STACK ";
+        
+        if ($VAL['NAME'] == 'size') {
+            #$def[$i] .= "AREA:var${KEY}#d4d4ff:\"". $name ."\" ";
+            #$def[$i] .= "HRULE:" .  . "#900:\"\" ";
+            $def[$i] .= "AREA:var${KEY}#ccccc9:\"". $name ."\" ";
+            $def[$i] .= "LINE1:var${KEY}#333333:\"\" ";
+        } else {
+            $def[$i] .= "LINE1:var${KEY}".rrd::color($KEY).":\"". $name ."\" ";
+        }
+        $def[$i] .= "GPRINT:var${KEY}:LAST:\"Last\: %8.1lf MB\" ";
+        $def[$i] .= "GPRINT:var${KEY}:MAX:\"Max\: %8.1lf MB\" ";
+        $def[$i] .= "GPRINT:var${KEY}:AVERAGE:\"Average\: %8.1lf MB\" ";
         $def[$i] .= "COMMENT:\"\\n\" ";
     }
 }
 
-
-return;
-
+# graph fpr compression-ratio
+++$i;
+$ds_name[$i] = "vol-sizes";
+$def[$i]  = "";
+$opt[$i]  = " --vertical-label 'compression ratio %' --title '" . $this->MACRO['DISP_HOSTNAME'] . " / $zvol_name' -l 0";
 foreach ($this->DS as $KEY=>$VAL) {
-
-
-    $maximum  = "";
-    $minimum  = "";
-    $critical = "";
-    $crit_min = "";
-    $crit_max = "";
-    $warning  = "";
-    $warn_max = "";
-    $warn_min = "";
-    $vlabel   = " ";
-    $lower    = "";
-    $upper    = "";
-
-    if ($VAL['WARN'] != "" && is_numeric($VAL['WARN']) ){
-        $warning = $VAL['WARN'];
+    if ($VAL['NAME'] == 'comp_ratio') {
+        $def[$i] .= "DEF:var${KEY}=${VAL['RRDFILE']}:${DS[$VAL['DS']]}:AVERAGE ";
+        $def[$i] .= "LINE1:var${KEY}".rrd::color($KEY).":\"".$VAL['NAME']."\" ";
+        $def[$i] .= "GPRINT:var${KEY}:LAST:\"Last\: %5.2lf %% \" ";
+        $def[$i] .= "GPRINT:var${KEY}:MAX:\"Max\: %5.2lf %% \" ";
+        $def[$i] .= "GPRINT:var${KEY}:AVERAGE:\"Average\: %5.2lf %% \" ";
+        $def[$i] .= "COMMENT:\"\\n\" ";
     }
-    if ($VAL['WARN_MAX'] != "" && is_numeric($VAL['WARN_MAX']) ) {
-        $warn_max = $VAL['WARN_MAX'];
-    }
-    if ( $VAL['WARN_MIN'] != "" && is_numeric($VAL['WARN_MIN']) ) {
-        $warn_min = $VAL['WARN_MIN'];
-    }
-    if ( $VAL['CRIT'] != "" && is_numeric($VAL['CRIT']) ) {
-        $critical = $VAL['CRIT'];
-    }
-    if ( $VAL['CRIT_MAX'] != "" && is_numeric($VAL['CRIT_MAX']) ) {
-        $crit_max = $VAL['CRIT_MAX'];
-    }
-    if ( $VAL['CRIT_MIN'] != "" && is_numeric($VAL['CRIT_MIN']) ) {
-        $crit_min = $VAL['CRIT_MIN'];
-    }
-    if ( $VAL['MIN'] != "" && is_numeric($VAL['MIN']) ) {
-        $lower = " --lower=" . $VAL['MIN'];
-        $minimum = $VAL['MIN'];
-    }
-    if ( $VAL['MAX'] != "" && is_numeric($VAL['MAX']) ) {
-        $maximum = $VAL['MAX'];
-    }
-    if ($VAL['UNIT'] == "%%") {
-        $vlabel = "%";
-        $upper = " --upper=101 ";
-        $lower = " --lower=0 ";
-    }
-    else {
-        $vlabel = $VAL['UNIT'];
-    }
-
-    $opt[$KEY] = '--vertical-label "VL' . $vlabel . '" --title "' . $this->MACRO['DISP_HOSTNAME'] . ' / ' . $this->MACRO['DISP_SERVICEDESC'] . '"' . $upper . $lower;
-    $ds_name[$KEY] = $VAL['LABEL'] . '-X';
-    $def[$KEY]  = rrd::def     ("var1", $VAL['RRDFILE'], $VAL['DS'], "AVERAGE");
-    $def[$KEY] .= rrd::gradient("var1", "ccccff", "cccc44", rrd::cut($VAL['NAME'],16), 20);
-    $def[$KEY] .= rrd::line1   ("var1", $_LINE );
-    #$def[$KEY] .= rrd::gprint  ("var1", array("LAST","MAX","AVERAGE"), "%3.4lf %S".$VAL['UNIT']);
-    $def[$KEY] .= rrd::gprint  ("var1", array("LAST","MAX","AVERAGE"), "%3.0lf MB");
-    if ($warning != "") {
-        $def[$KEY] .= rrd::hrule($warning, $_WARNRULE, "Warning  $warning \\n");
-    }
-    if ($warn_min != "") {
-        $def[$KEY] .= rrd::hrule($warn_min, $_WARNRULE, "Warning  (min)  $warn_min \\n");
-    }
-    if ($warn_max != "") {
-        $def[$KEY] .= rrd::hrule($warn_max, $_WARNRULE, "Warning  (max)  $warn_max \\n");
-    }
-    if ($critical != "") {
-        $def[$KEY] .= rrd::hrule($critical, $_CRITRULE, "Critical $critical \\n");
-    }
-    if ($crit_min != "") {
-        $def[$KEY] .= rrd::hrule($crit_min, $_CRITRULE, "Critical (min)  $crit_min \\n");
-    }
-    if ($crit_max != "") {
-        $def[$KEY] .= rrd::hrule($crit_max, $_CRITRULE, "Critical (max)  $crit_max \\n");
-    }
-    $def[$KEY] .= rrd::comment("Template " . $VAL['TEMPLATE'] . "\\r");
 }
-?>
-
-
